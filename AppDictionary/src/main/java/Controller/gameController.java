92f801c47d606ff949cab2c17ad314dee08b9d97
22021191 <132531766+22021191@users.noreply.github.com>
2023-11-18 00:35:10 +0700

    meada

--- AppDictionary/src/main/java/Controller/gameController.java
+++ AppDictionary/src/main/java/Controller/gameController.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import Base.game.Question;
+import Base.game.QuestionLoader;
 import Base.game.Bullet.Bullet;
 import Base.game.Bullet.IceBullet;
 import Base.game.GameState;
@@ -13,10 +15,13 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.ParallelTransition;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -71,6 +76,31 @@ public class gameController implements Initializable {
     public static List<Lawn_mower> lawnMovers = new ArrayList<Lawn_mower>();
     public static List<Card> plantIterm = new ArrayList<Card>();
     public DataZombie zombie = new DataZombie();
+
+    /**
+     * ket.
+     */
+    @FXML
+    private ToggleGroup Choice;
+
+    @FXML
+    private Label lblQuestion;
+
+    @FXML
+    private RadioButton rdoA;
+
+    @FXML
+    private RadioButton rdoB;
+
+    @FXML
+    private RadioButton rdoC;
+
+    private List<Question> questions;
+    private int currentQuestionIndex = 0;
+
+
+
+
     public void ShowData() {
         PausePane.setVisible(true);
         PausePane.setViewOrder(-2);
@@ -445,6 +475,11 @@ public class gameController implements Initializable {
                 UpdateLevel
         );
         ShowData();
+        try {
+            questions = QuestionLoader.loadQuestions("src/main/resources/asset/question.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         GameRoot.setVisible(true);
         BoxPlant.setViewOrder(-1);
         imageView.setImage(new Image("/asset/Game/Lawn.png"));
@@ -496,6 +531,7 @@ public class gameController implements Initializable {
                 imageView.setImage(new Image("/asset/Game/cg18.png"));
                 imageView.setOnMouseClicked(event->{
                     GamePlay();
+                    showQuestion();
                 });
 
             } catch (InterruptedException e) {
@@ -505,5 +541,50 @@ public class gameController implements Initializable {
 
         game.start();
 
+    }
+
+    /**
+     * ket.
+     */
+    @FXML
+    void checkYourAnswer(ActionEvent event) {
+        Question currentQuestion = questions.get(currentQuestionIndex);
+        RadioButton selectedRadioButton = (RadioButton) Choice.getSelectedToggle();
+        String selectedAnswer = selectedRadioButton.getText();
+
+        if (selectedAnswer.equals(currentQuestion.getCorrectAnswer())) {
+            UpdateSunCount(50);
+            showSun();
+            nextAnswer(event);
+        } else {
+            System.out.println("Incorrect!");
+            nextAnswer(event);
+        }
+    }
+
+    @FXML
+    void nextAnswer(ActionEvent event) {
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < questions.size()) {
+            showQuestion();
+        } else {
+            System.out.println("End of questions.");
+        }
+    }
+    private void showQuestion() {
+        Question currentQuestion = questions.get(currentQuestionIndex);
+
+        lblQuestion.setText(currentQuestion.getQuestion());
+
+        List<String> choices = currentQuestion.getChoices();
+
+        if (choices != null && choices.size() >= 3) {
+            rdoA.setText(choices.get(0));
+            rdoB.setText(choices.get(1));
+            rdoC.setText(choices.get(2));
+        } else {
+            System.err.println("Error: Choices list does not have enough elements.");
+        }
     }
 }
